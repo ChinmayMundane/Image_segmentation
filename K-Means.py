b1a1848089a7d6dbfd14e0b7585771d84331d33e
ChinmayMundane <109454803+ChinmayMundane@users.noreply.github.com>
2022-08-31 23:11:33 +0530

    Update K-Means.py
    added iterative approach to the existing code

--- K-Means.py
+++ K-Means.py
@@ -1,6 +1,5 @@
 from re import X
 import numpy as np
-import matplotlib.pyplot as plt
 import math as m
 import cv2
 
@@ -17,6 +16,8 @@ print('init clusters', Clusters)
 img=image.copy()
 diff = np.zeros(K,int)
 a1 = np.zeros([K,X],int)
+array_check = np.zeros([K,c],bool)
+
 
 # initialized random K points forming K centroids
 # and assigned each point of the image to that centroid
@@ -34,24 +35,45 @@ for i in range(h):
         cent= np.argmin(diff)
         img[i][j]=Clusters[cent]
 
+flag = 0
+
 # Compute the centroids for the clusters by taking 
 # the average of the all data points that belong to each cluster
 # and assigning the points to that new cluster formed, if any.
+while(flag == 0):
+    for m in range(K):
+        arr = np.array(Clusters[m])
+        for c in range(X):
+            a1[m,c]=int(((np.sum(arr[c: :X])) / ((len(Clusters[m]))/X)))
 
-for m in range(K):
-    arr = np.array(Clusters[m])
-    for c in range(X):
-        a1[m,c]=int(((np.sum(arr[c: :X])) / ((len(Clusters[m]))/X)))
-
-for i in range(h):
-    for j in range(w):
-        pnt=img[i][j]
     
-        for l in range(K):
-            diff[l]=np.sqrt(np.sum(a1[l]-pnt)**2)
+
+
+    for i in range(h):
+        for j in range(w):
+            pnt=img[i][j]
+    
+            for l in range(K):
+                diff[l]=np.sqrt(np.sum(a1[l]-pnt)**2)
             
-        cent= np.argmin(diff)
-        img[i][j]=a1[cent]
+            cent= np.argmin(diff)
+            img[i][j]=a1[cent]
+
+    for t in range(K):
+            for p in range(X):
+                if((a1[t,p]-Clusters[t,p])<5):
+                    array_check[t,p] = True
+
+    if(array_check.all()==False):
+        flag = 0
+        for q in range(K):
+            Clusters[q]=a1[q]
+    else:
+        break
+        
+            
+    
+
 
 
 if __name__ == '__main__':
