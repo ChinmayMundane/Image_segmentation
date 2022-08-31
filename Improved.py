2d92a3c93cd342ee9644c36ae740f9388ecd0038
Smit1603 <smuffinboy16@gmail.com>
2022-08-31 14:06:41 +0530

    Optimized K-Means Code To Resolve Time Complexity

---
+++ Improved.py
@@ -0,0 +1,147 @@
+import random 
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import math as m
+
+
+# To Calculate Distance
+def distance(array , array1 , a ):
+    sum = 0
+    for p in range(a):
+        sum += (array[p] - array1[p])**2
+    b = m.sqrt(sum)
+    dis = int(b)
+    return dis
+
+
+
+# Reading Image
+def reader():
+    
+    image = plt.imread("/home/smit/Desktop/Trial.jpg")
+    image1 = np.copy(image)
+    plt.imshow(image)
+    plt.show()
+    
+    K1 = input("Enter Number Of Clusters : ")
+    K = int(K1)
+    
+    h,w,c = image.shape[0],image.shape[1],image.ndim
+    
+    a = np.zeros([K,c],int) # Array To Store Old Cluster Centroids
+    a1 = np.zeros([K,c],int) # Array To Store New Cluster Centroids
+    array_check = np.zeros([K,c],bool) # Array To Check Difference In Old And New Cluster Centroids
+
+    for pos in range(K): # To Randomly Select K Initial Cluster Centroid
+        for i in range(c):
+            r_x = random.randint(0,h-1)
+            r_y = random.randint(0,w-1)
+            a[pos,i]=image[r_x,r_y,i]
+
+    dis = np.zeros(c,int)
+    cendis = np.zeros(K,int)
+    flag = 0
+
+    count = 0
+    sdis = [] # 1st Data Type To Store Inital Distance
+    cluster = [] # 2nd Data Type To Store Cluster Label
+
+    while(flag == 0):
+        
+        clust_x = []
+        clust_y = []
+        clust = []
+        for y in range (K):
+            clust_x.append([])
+            clust_y.append([])
+            clust.append([])
+        counter = 0
+        for i in range(h):
+            for j in range(w):
+                
+                if(count!=0):
+
+                    old_cluster = cluster[counter]
+                    for chan in range(c):
+                        dis[chan] = image[i,j,chan]
+                    find = distance(dis , a[old_cluster] , c)
+                    
+                    if(find <= sdis[counter]):
+                        cluster[counter] = old_cluster
+                        sdis[counter] = find
+                        clust_x[old_cluster].append(i)
+                        clust_y[old_cluster].append(j)
+                        for chan in range(c):
+                            clust[old_cluster].append(image[i,j,chan])
+
+                    else:
+                        for l in range(K):
+                            for chan in range(c):
+                                dis[chan] = image[i,j,chan]
+                            cendis[l] = distance(dis , a[l] , c )
+                        sdis[counter]=min(cendis) # 1st Data Type To Store Inital Distance
+                        mdis = min(cendis)
+
+                        for k in range(K):
+                            if(mdis == cendis[k]):
+                                clust_x[k].append(i)
+                                clust_y[k].append(j)
+                                cluster[counter]=k # 2nd Data Type To Store Cluster Label
+                                for chan in range(c):
+                                    clust[k].append(image[i,j,chan])
+                       
+                else : 
+                    
+                    for l in range(K):
+                        for chan in range(c):
+                            dis[chan] = image[i,j,chan]
+                        cendis[l] = distance(dis , a[l] , c )
+                    sdis.append(min(cendis)) # 1st Data Type To Store Inital Distance
+                    mdis = min(cendis)
+
+                    for k in range(K):
+                        if(mdis == cendis[k]):
+                            clust_x[k].append(i)
+                            clust_y[k].append(j)
+                            cluster.append(k) # 2nd Data Type To Store Cluster Label
+                            for chan in range(c):
+                                clust[k].append(image[i,j,chan])
+                    continue
+                counter+=1
+
+        count=1
+        # To Find New Cluster Centroid By Taking Mean
+        for m in range(K):
+            arr = np.array(clust[m])
+            for chan in range(c):
+                a1[m,chan]=int(((np.sum(arr[chan : :c])) / ((len(clust[m]))/c)))
+        
+        # To Check Whether New Cluster Centroid Is Similar To Old One Or Not
+        print("Iteration Complete")
+        for t in range(K):
+            for p in range(c):
+                if((a1[t,p]-a[t,p])<5 and (a1[t,p]-a[t,p])>-5):
+                    array_check[t,p] = True
+
+        if(array_check.all()==True):
+            flag = 1
+        else:
+            flag = 0
+            for q in range(K):
+                a[q]=a1[q]
+
+    # To Print Final Image
+    store = np.zeros(c,int)
+    for t in range(K):
+        for p in range(len(clust_x[t])):
+            for chan in range(c):
+                store[chan] = a1[t,chan]
+            image1[clust_x[t][p],clust_y[t][p]] = store
+
+    plt.imshow(image1)
+    plt.show()
+    plt.imshow(image)
+    plt.show()
+
+reader()
