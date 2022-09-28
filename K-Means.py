90a7eda45f0403812196ca8fcd3e73b82e97c1f9
ChinmayMundane <109454803+ChinmayMundane@users.noreply.github.com>
2022-09-28 12:04:28 +0530

    updated k-means

--- K-Means.py
+++ K-Means.py
@@ -1,82 +1,75 @@
-from re import X
 import numpy as np
 import math as m
 import cv2
-
-
-image=cv2.imread("I:\image segmentation\image1.png")
-K1 = input("No. Of Clusters : ")
-K = int(K1)
-h,w,c=image.shape
-X = image.ndim
-
-Clusters=np.random.randint(0,255,size=(K,3))
+import time
+K = int(input("Enter Number Of Clusters : "))
+image=cv2.imread("tester5.png")
+h,w,c=image.shape[0],image.shape[1],image.shape[2]
+Clusters=np.random.randint(0,255,size=(K,c))
 print('init clusters', Clusters)
-
-img=image.copy()
-diff = np.zeros(K,int)
-a1 = np.zeros([K,X],int)
+#img=image.copy()
+img = image.copy()
+dist = np.zeros(K,int)
+centd = np.zeros(K,int)
+new_cent = np.zeros([K,c],float)
 array_check = np.zeros([K,c],bool)
-# initialized random K points forming K centroids
-# and assigned each point of the image to that centroid
-# based on the difference of the points to clusters 
-# and assigning points to that centroid which has lowest
-# distance between itself and points.
-
-for i in range(h):
-    for j in range(w):
-        pnt=img[i][j]
-    
-        for l in range(K):
-            diff[l]=np.sqrt(np.sum(Clusters[l]-pnt)**2)
-            
-        cent= np.argmin(diff)
-        img[i][j]=Clusters[cent]
-
-flag = 0
-
-# Compute the centroids for the clusters by taking 
-# the average of the all data points that belong to each cluster
-# and assigning the points to that new cluster formed, if any.
-while(flag == 0):
-    for m in range(K):
-        arr = np.array(Clusters[m])
-        for c in range(X):
-            a1[m,c]=int(((np.sum(arr[c: :X])) / ((len(Clusters[m]))/X)))
-
-    
-
-
+start = time.time()
+R=0
+while R==0:
+    b = []
+    b1 = []
+    b2 =[]
+    for y in range (K):
+        b.append([])
+        b1.append([])
+        b2.append([])
     for i in range(h):
         for j in range(w):
             pnt=img[i][j]
-    
             for l in range(K):
-                diff[l]=np.sqrt(np.sum(a1[l]-pnt)**2)
-            
-            cent= np.argmin(diff)
-            img[i][j]=a1[cent]
+                dist[l]=np.sqrt(np.sum((Clusters[l]-pnt)**2))
+                    #dist[chan] = img[i,j,chan]
+            centd = min(dist)
 
+            for k in range(K):
+                if(centd==dist[k]):
+                    b1[k].append(i)
+                    b[k].append(j)
+                    for chan in range(c):
+                        b2[k].append(img[i,j,chan])
+                        
+
+    for m in range(K):
+        arr = np.array(b2[m])
+        for chan in range(c):    
+            new_cent[m,chan]=(((np.sum(arr[chan : :c])) / ((len(arr))/c)))
     for t in range(K):
-            for p in range(X):
-                if((a1[t,p]-Clusters[t,p])<5):
+            for p in range(c):
+                if((new_cent[t,p]-Clusters[t,p]<1) and (new_cent[t,p]-Clusters[t,p]>-1)):
                     array_check[t,p] = True
 
-    if array_check[t,p] ==False in range(t-1,p-1):
-        flag = 0
-        for q in range(K):
-            Clusters[q]=a1[q]
+
+    if(array_check.all() == True):
+        R=1
     else:
-        break
+        R=0
+        for q in range(K):
+            Clusters[q]=new_cent[q]
+            print("iterating")
 
-      
-    
-if __name__ == '__main__':
-	cv2.imshow('original_image',image)
-	cv2.imshow('cluster_image',img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+key = np.zeros(c,float)
+for t in range(K):
+    for p in range(len(b1[t])):
+        for chan in range(c):
+            key[chan] = new_cent[t,chan]
+            img[b1[t][p],b[t][p]] = key
+end = time.time()
+print("The time of execution of above program is :",(end-start), "s")
 
+cv2.imshow('original_image',image)
+cv2.imshow('cluster_image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
