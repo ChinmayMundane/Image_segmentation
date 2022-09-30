766f692a67153d14f1519061777f800f734b78b6
Smit1603 <smuffinboy16@gmail.com>
2022-09-30 16:43:35 +0530

    Updated Files

--- pcs.py
+++ pcs.py
@@ -1,8 +1,20 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt 
 
-def pcs(image):
+def PCS(image):
+    """
+    Description : The Given Function 'PCS' Is Used To Perform Pre Processing On Image To Attain An Aim Of Getting Better Output
+    Parameters : The Following Function Has 1 Parameter 
+                 K - It Tells Number Of Cluster To Be Formed / Number Of Initial Centroid To Be Generated
+    Formulae : In This Method We Try To Store 10 Or More Sets Of K Cluster Centroid And We Calculate Sum Of Square 
+               Of Distance Between Initial Centroids Choosen For Each Set
+               Example : Suppose A Set Has 4 Cluster Centroids Than We Try To Collect 10 Or More Sets With 4 Cluster Centroid
+                         And Then We Try To Calculate Sum OF Square Of Distance Between Each Of The 4 Cluster Centroid
+                         And Only That Set Of Initial Cluster Centroid Is Choosen Where The Sum Is Maximum
+    Variables : cluster - 'cluster' Is List Used To Store 10 Or More Set Of Inital Cluster Centroid
+                sum - 'sum' Is List Used To Store Sum Of Square Of Distances Calculated
+                       For Each Set
+    Returns : It Returns Variable 'initial_centroids' Which Stores A List Of Initial Cluster Centroid
+    """
     new_max = 240
     new_min = 70
     image1 = np.copy(image)
