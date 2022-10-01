10ce71d6a02c61def97b9a50f9ebfd80bb7deb03
Smit1603 <smuffinboy16@gmail.com>
2022-10-01 12:21:26 +0530

    Adding Main File And Modified PCS Code

---
+++ main.py
@@ -0,0 +1,24 @@
+from random1 import RANDOM
+from KMeans_Plus import K_MEANS_PLUS
+from Subtractive_Clustering import SUBTRACTIVE
+from K_Means import K_MEANS
+from Improved import K_MEANS_IMPROVED
+from pcs import PCS
+
+def main():
+    print("To Use K-Means Improved Method -----> 1")
+    print("To Use K-Means Method -----> 2")
+    a = int(input("Input : "))
+    K = int(input("Enter Number Of Clusters : "))
+    path = input("Enter Image Path : ")
+    print("To Use Subtractive Clustering Method -----> 0")
+    print("To Use Random Initialization Method -----> 1")
+    print("To Use K-Means++ Method -----> 2")
+    b = input("Input : ")
+    if(a==1):
+        K_MEANS_IMPROVED(path,K,b)
+    else:
+        K_MEANS(path,K,b)
+
+if __name__=="__main__":
+    main()
--- pcs.py
+++ pcs.py
@@ -1,22 +1,18 @@
 import numpy as np
 
-def PCS(image):
+def PCS(image,new_max,new_min):
     """
-    Description : The Given Function 'PCS' Is Used To Perform Pre Processing On Image To Attain An Aim Of Getting Better Output
+    Description : The Given Function 'PCS' Is Used To Perform Pre Processing On Image To Attain An Aim Of 
+                  Getting Better Clustering Result
     Parameters : The Following Function Has 1 Parameter 
-                 K - It Tells Number Of Cluster To Be Formed / Number Of Initial Centroid To Be Generated
-    Formulae : In This Method We Try To Store 10 Or More Sets Of K Cluster Centroid And We Calculate Sum Of Square 
-               Of Distance Between Initial Centroids Choosen For Each Set
-               Example : Suppose A Set Has 4 Cluster Centroids Than We Try To Collect 10 Or More Sets With 4 Cluster Centroid
-                         And Then We Try To Calculate Sum OF Square Of Distance Between Each Of The 4 Cluster Centroid
-                         And Only That Set Of Initial Cluster Centroid Is Choosen Where The Sum Is Maximum
-    Variables : cluster - 'cluster' Is List Used To Store 10 Or More Set Of Inital Cluster Centroid
-                sum - 'sum' Is List Used To Store Sum Of Square Of Distances Calculated
-                       For Each Set
-    Returns : It Returns Variable 'initial_centroids' Which Stores A List Of Initial Cluster Centroid
+                 image - It Returns Numpy Array Of Image 
+                 new_max - It Return New Maximum Value Of Pixel
+                 new_min - It Return New Minimum Value Of Pixel
+    Formulae : In This Method We Have Implemented Partial Compression And Stretching Algorithm
+    Variables : pixel - 'pixel' Is Array Used To Store RGB Values Of Image
+                new_pixel- 'new_pixel' Is Array Used To Store New Modified Pixel Value
+    Returns : It Returns 'image1'  Which Stores A Numpy Array Of Modified Image
     """
-    new_max = 240
-    new_min = 70
     image1 = np.copy(image)
     h,w,c = image.shape[0],image.shape[1],image.shape[2]
     pixel = np.zeros(c,float)
@@ -28,5 +24,4 @@ def PCS(image):
                 new_pixel = (((new_max-new_min)*(pixel))/255) + new_min
             for chan in range(c):
                 image1[i,j,chan]=new_pixel[chan]
-    
     return image1
